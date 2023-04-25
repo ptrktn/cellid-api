2b1197ebc95f18a5cdabf5fa74ea1991c46bd96c
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-04-26 07:44:12 +0900

    Add OpenCelliD data parsing

--- Makefile
+++ Makefile
@@ -2,8 +2,12 @@
 all:
 	false
 
-.PHONY: db
-db:
+.PHONY: bootstrap
+bootstrap:
 	rails generate model Cell mcc:integer mnc:integer lac:integer cid:integer longitude:float latitude:float --force
 	rails db:migrate
 	rails g controller api/v1/Cells --force
+
+.PHONY: db
+db:
+	bin/rails db:seed:replant
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+# Example of a row in OpenCellID data.
+# LTE,441,10,45060,257415168,-1,135.332863,34.388399,1000,57,1,1552867238,1575588586,0
+
+
+if ENV['OPENCELLID_DATA_DIR'] && Dir.exist?(ENV['OPENCELLID_DATA_DIR'])
+  Dir[File.join(ENV['OPENCELLID_DATA_DIR'], '*.csv')].each do |fname|
+    puts "Processing #{fname}"
+    data = CSV.read(fname)
+    data.each do |row|
+      Cell.create!(
+        mcc: row[1],
+        mnc: row[2],
+        lac: row[3],
+        cid: row[4],
+        longitude: row[6],
+        latitude: row[7]
+      )
+    end
+  end
+else
+  puts "OpenCelliD data directory is undefined"
+end
